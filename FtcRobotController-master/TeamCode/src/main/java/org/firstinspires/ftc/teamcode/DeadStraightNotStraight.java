8f5a0e94f39bd552bcd91688a05d100734f77c05
Ayden-Jezak-Robotics <aydenjezak@lenoxps.org>
2024-10-28 15:39:10 -0400

    commit

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeadStraightNotStraight.java
@@ -0,0 +1,130 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Manual PID ", group = "Concept")
+public class DeadStraightNotStraight extends LinearOpMode {
+
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor rightDeadWheel;
+    private DcMotor leftDeadWheel;
+    private double motorTicks;
+    private double currentValue;
+    private double leftCurrentValue;
+    private double rightCurrentValue;
+    private double deltaTime;
+    private PIDController PIDController;
+    private PIDController leftPIDController;
+    private PIDController rightPIDController;
+    private ElapsedTime timer;
+    private int ticksPerRotation = 8192;
+    double wheelCircumference = (Math.PI * 60);
+    double inchesPerTick = (wheelCircumference/ticksPerRotation)/2.54;
+    double ticksPerInch = (ticksPerRotation/wheelCircumference)*25.4;
+    double leftOutput, rightOutput;
+
+    @Override
+    public void runOpMode() {
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontRight");
+        rightDeadWheel = hardwareMap.get(DcMotor.class, "rightDeadWheel");
+        leftDeadWheel = hardwareMap.get(DcMotor.class,"leftDeadWheel");
+
+
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightDeadWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDeadWheel.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        rightDeadWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDeadWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightDeadWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDeadWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+       // PIDController = new PIDController(0.00002, 0, 0, 0.1);
+        leftPIDController = new PIDController(0.005, 0, 0, 0.1);
+        rightPIDController = new PIDController(0.005, 0, 0, 0.1);
+
+        //kP= 0.00003, kI= 0.000003, kD= 0.000005
+
+        timer = new ElapsedTime();
+        waitForStart();
+        while(opModeIsActive()){
+            setPosition(12);
+            timer.reset();
+            leftPIDController.update(0,deltaTime );
+            rightPIDController.update(0,deltaTime);
+            telemetry.addData("Before Loop", "");
+            telemetry.update();
+            setMotorPower(0.1);
+            while (Math.abs(leftPIDController.error) >= leftPIDController.tolerance && Math.abs(rightPIDController.error) >= rightPIDController.tolerance)
+            {
+                leftCurrentValue = leftDeadWheel.getCurrentPosition();
+                rightCurrentValue = rightDeadWheel.getCurrentPosition();
+                deltaTime = timer.seconds();
+                leftOutput = leftPIDController.update(leftCurrentValue,deltaTime);
+                rightOutput = rightPIDController.update(rightCurrentValue,deltaTime);
+                setIndividualMotorPower(leftOutput,rightOutput);
+                doTelemetryData();
+            }
+            setMotorPower(0);
+            reset();
+        }
+    }
+
+    private void setIndividualMotorPower(double leftMotorPower, double rightMotorPower){
+        backLeft.setPower(leftMotorPower);
+        backRight.setPower(rightMotorPower);
+        frontLeft.setPower(leftMotorPower);
+        frontRight.setPower(rightMotorPower);
+
+    }
+
+    private void doTelemetryData(){
+        telemetry.addData("Left Wheel", leftDeadWheel.getCurrentPosition());
+        telemetry.addData("Right Wheel", rightDeadWheel.getCurrentPosition());
+        telemetry.addData("Target Ticks", motorTicks);
+        telemetry.addData("ElapsedTime", deltaTime);
+        telemetry.addData("Left Error", leftPIDController.error);
+        telemetry.addData("Right Error", rightPIDController.error);
+        telemetry.addData("Left Current Value", leftCurrentValue);
+        telemetry.addData("Right Current Value", rightCurrentValue);
+        telemetry.addData("Tolerance", leftPIDController.tolerance);
+        telemetry.addData("Tolerance", rightPIDController.tolerance);
+        telemetry.addData("Motor Power Left", leftOutput);
+        telemetry.addData("Motor Power Right", rightOutput);
+        telemetry.update();
+    }
+
+    private void setMotorPower(double motorPower){
+        backLeft.setPower(motorPower);
+        backRight.setPower(motorPower);
+        frontLeft.setPower(motorPower);
+        frontRight.setPower(motorPower);
+
+    }
+    private void reset(){
+        rightDeadWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDeadWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDeadWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDeadWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    private void setPosition(double inches) {
+        // I hate you - Cruz Swinson
+        motorTicks = inches * ticksPerInch;
+        leftPIDController.setTargetAmount(motorTicks);
+        rightPIDController.setTargetAmount(motorTicks);
+    }
+}
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -5,16 +5,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class PIDController {
 
-    public double kP, kI, kD; // PID gains
+    public double kP, kI, kD, kF; // PID gains
     public double integral, previousError, derivative, error;
     private double targetAmount; // Desired value
     private double maxOutput = 1; // Maximum allowable motor power
-    private double rampRate = 0.1; // Rate at which the power ramps up
+    private double output;
+    //private double rampRate = 0.1;// Rate at which the power ramps up
+    public double tolerance = 50;
 
-    public PIDController(double kP, double kI, double kD) {
+    public PIDController(double kP, double kI, double kD, double kF) {
         this.kP = kP;
         this.kI = kI;
         this.kD = kD;
+        this.kF = kF;
         this.integral = 0;
         this.previousError = 0;
         this.targetAmount = 0;
@@ -24,47 +27,22 @@ public class PIDController {
         this.targetAmount = targetAmount;
     }
 
-    // Sai
-    public void makeStraight(DcMotor leftOne, DcMotor rightOne)
-    {
-
-    }
-    //
-
     public double update(double currentValue, double deltaTime) {
         error = (targetAmount - currentValue);
         integral += error * deltaTime;
         derivative = (error - previousError) / deltaTime;
+        double accelTicks = targetAmount/4;
 
-        double baseOutput = (kP * error) + (kI * integral) + (kD * derivative);
-
-        double output = baseOutput;
-
+        double baseOutput = (kP * error) + (kI * integral) + (kD * derivative) + kF;
+        while (currentValue < accelTicks){
+            output = baseOutput * (currentValue/accelTicks);
+            return clampOutput(output);
+        }
+        output = baseOutput;
         previousError = error;
+
         return clampOutput(output);
     }
-
-    /*
-    private double rampUpOutput(double baseOutput, double deltaTime) {
-        double targetOutput = baseOutput;
-        double currentOutput = previousOutput;
-
-        // Calculate the maximum change in output based on the ramp rate
-        double maxChange = rampRate * deltaTime;
-
-        // Calculate the new output by ramping up
-        if (targetOutput > currentOutput) {
-            currentOutput += maxChange;
-        } else {
-            currentOutput = targetOutput; // Directly set to target if ramping down
-        }
-
-        previousOutput = currentOutput; // Update the previous output
-        return currentOutput;
-    }
-    */
-
-
     public double clampOutput(double output) {
         return Math.max(-maxOutput, Math.min(maxOutput, output));
     }
